44e54aacb422b48b975cde86b05329705abb57e9
k0baya <132335755+k0baya@users.noreply.github.com>
2023-06-19 16:42:49 +0800

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/holy-unblocker/website-aio server && rm -f server/.replit && rm -f server/replit.nix && rm -f server/README.md && rm -f server/LICENSE
+rm -rf server && git clone https://github.com/holy-unblocker/website-aio server && rm -f server/.replit && rm -f server/replit.nix && rm -f server/README.md && rm -f server/LICENSE
