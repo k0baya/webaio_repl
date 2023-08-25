0b328419da3a724a76e520b4e6ccd1322840fbed
k0baya <132335755+k0baya@users.noreply.github.com>
2023-08-25 19:38:50 +0800

    fix some errors

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
-cd server && npm install && npm audit fix && npm audit && npm install && nohup npm start >/dev/null 2>&1 &
+cd server && npm install ;
+
+if [ ! -f "done" ]; then
+  npm audit 
+  npm audit fix
+  npm install
+  echo "1" > done
+fi
+&& nohup npm start >/dev/null 2>&1 &
+
 while true ; do
   sleep 10
   echo "测试运行中"
